5aa12fad608c2119892b5e119af88b726696962a
x-tro <s-nesterko@mail.ru>
2014-07-23 10:15:02 +0400

    23/07

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  subject { page }
+
+  describe "Home page" do
+    before { visit root_path }
+
+    it { should have_content('Sample App') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
+  end
+
+  describe "Help page" do
+    before { visit help_path }
+
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
+  end
+
+  describe "About page" do
+    before { visit about_path }
+
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
+  end
+
+  describe "Contract page" do
+    before { visit contract_path }
+
+    it { should have_content('Contract') }
+    it { should have_title(full_title('Contract')) }
+  end
+end
